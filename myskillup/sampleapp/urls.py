d20e2a773d90f8496f420c3ac3d84e8a43f3ab06
patelrakesh95 <patel_rakesh95@outlook.com>
2020-07-06 17:45:34 +0900

    Handling form data in python.

--- myskillup/sampleapp/urls.py
+++ myskillup/sampleapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='sampleapp/index'),
     path('index/',views.renderIndex,name='sampleapp/renderIndex'),
-    path('indexmsg/',views.custommsg,name='sampleapp/custommsg')
+    path('indexmsg/',views.custommsg,name='sampleapp/custommsg'),
+    path('input/',views.usermsg,name='sampleapp/usermsg')
 ]
--- myskillup/sampleapp/views.py
+++ myskillup/sampleapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from  django.http import request,HttpResponse
+from django.http import request,HttpResponse
 # Create your views here.
 
 def index(request):
@@ -14,3 +14,13 @@ def custommsg(request):
         'message':"Custom message from python."
     }
     return render(request,'sampleapp/indexmsg.html',context)
+
+def usermsg(request):
+    name = None
+    if 'name' in request.POST:
+        name = request.POST['name']
+    context = {
+        'title':'User Message',
+        'name':name
+    }
+    return render(request,'sampleapp/input.html',context)
